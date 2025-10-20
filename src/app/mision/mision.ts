d378c8e0af7d2854d18345f6bea25308cafc1de0
Daniela <danielabuitragofajardo@gmail.com>
2025-10-20 14:37:16 -0500

    first commit

---
+++ src/app/mision/mision.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Header } from '../shared/components/header/header';
+
+@Component({
+  selector: 'app-mision',
+  imports: [Header],
+  templateUrl: './mision.html',
+  styleUrl: './mision.css'
+})
+export class Mision {
+
+}
